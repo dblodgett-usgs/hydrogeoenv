244f13c4ee1edee4e7baf48960c748a72400765e
David Blodgett <dblodgett@usgs.gov>
2020-07-13 07:20:20 -0500

    clean up

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ r_TAG=${2:-latest}
 custom_TAG=${3:-latest}
 push=${4:-FALSE}
 
-linux=dblodgett-usgs/hydrogeoenv-linux:$linux_TAG
-r=dblodgett-usgs/hydrogeoenv-r:$r_TAG
-custom=dblodgett-usgs/hydrogeoenv-custom:$custom_TAG
+linux=dblodgett/hydrogeoenv-linux:$linux_TAG
+r=dblodgett/hydrogeoenv-r:$r_TAG
+custom=dblodgett/hydrogeoenv-custom:$custom_TAG
 
 docker build -t $linux --build-arg BASE_CONTAINER=jupyter/minimal-notebook:76402a27fd13 linux/
-docker build -t $r --build-arg BASE_CONTAINER=dblodgett-usgs/hydrogeoenv-linux:$linux_TAG r/
-docker build -t $custom --build-arg BASE_CONTAINER=dblodgett-usgs/hydrogeoenv-r:$r_TAG custom/
+docker build -t $r --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-linux:$linux_TAG r/
+docker build -t $custom --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-r:$r_TAG custom/
 
 if [ "$push" == "TRUE" ]; then
   docker push $linux
