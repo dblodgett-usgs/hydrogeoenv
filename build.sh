16b0edd64782b503ed2f316b51ab4d8e459222d4
David Blodgett <dblodgett@usgs.gov>
2020-07-20 11:57:56 -0500

    refactor so linux doesn't get conda installed

--- build.sh
+++ build.sh
@@ -2,15 +2,18 @@
 
 linux_TAG=${1:-latest}
 r_TAG=${2:-latest}
-custom_TAG=${3:-latest}
-push=${4:-FALSE}
+python_TAG=${3:-latest}
+custom_TAG=${4:-latest}
+push=${5:-FALSE}
 
 linux=dblodgett/hydrogeoenv-linux:$linux_TAG
+python=dblodgett/hydrogeoenv-python:$python_TAG
 r=dblodgett/hydrogeoenv-r:$r_TAG
 custom=dblodgett/hydrogeoenv-custom:$custom_TAG
 
-docker build -t $linux --build-arg BASE_CONTAINER=jupyter/minimal-notebook:76402a27fd13 linux/
-docker build -t $r --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-linux:$linux_TAG r/
+docker build -t $linux --build-arg BASE_CONTAINER=ubuntu:bionic-20200630 linux/
+docker build -t $python --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-linux:$r_TAG python/
+docker build -t $r --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-python:$linux_TAG r/
 docker build -t $custom --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-r:$r_TAG custom/
 
 if [ "$push" == "TRUE" ]; then
