7478cd63e7cc75fce829b505226064883a499212
David Blodgett <dblodgett@usgs.gov>
2021-09-09 08:31:49 -0500

    rebuild to latest deps

--- build.sh
+++ build.sh
@@ -14,19 +14,42 @@ custom_TAG=${4:-latest}
 push=${5:-FALSE}
 
 nocache_l=${6:-false}
+if [ "$nocache_l" == "false" ]; then
+    nocache_l=" --no-cache"
+else
+    nocache_l=""
+fi
+
 nocache_p=${7:-false}
+if [ "$nocache_p" == "false" ]; then
+    nocache_p=" --no-cache"
+else
+    nocache_p=""
+fi
+
 nocache_r=${8:-false}
+if [ "$nocache_r" == "false" ]; then
+    nocache_r=" --no-cache"
+else
+    nocache_r=""
+fi
+
 nocache_c=${9:-false}
+if [ "$nocache_c" == "false" ]; then
+    nocache_c=" --no-cache"
+else
+    nocache_c=""
+fi
 
 linux=dblodgett/hydrogeoenv-linux:$linux_TAG
 python=dblodgett/hydrogeoenv-python:$python_TAG
 r=dblodgett/hydrogeoenv-r:$r_TAG
 custom=dblodgett/hydrogeoenv-custom:$custom_TAG
 
-docker build -t $linux --build-arg BASE_CONTAINER=ubuntu:20.10 --no-cache=$nocache_l linux/
-docker build -t $python --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-linux:$r_TAG --no-cache=$nocache_p python/
-docker build -t $r --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-python:$linux_TAG --no-cache=$nocache_r r/
-docker build -t $custom --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-r:$r_TAG --no-cache=$nocache_c custom/
+docker build -t $linux --build-arg BASE_CONTAINER=ubuntu:20.10$nocache_l linux/
+docker build -t $python --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-linux:$r_TAG$nocache_p python/
+docker build -t $r --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-python:$linux_TAG $nocache_r r/
+docker build -t $custom --build-arg BASE_CONTAINER=dblodgett/hydrogeoenv-r:$r_TAG$nocache_c custom/
 
 if [ "$push" == "TRUE" ]; then
   docker push $linux
